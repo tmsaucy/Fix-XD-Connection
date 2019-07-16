2141154e2318154864bba2bb1b13cac6aaa8dca5
tmsaucy <tmcgrath24@gmail.com>
2019-07-16 09:19:25 -0400

    Add files via upload

---
+++ Fix XD Connection Issues.ps1
@@ -0,0 +1,5 @@
+﻿$computer=read-host "Enter XenDesktop name, that is having connection issues: "
+Get-Service -Name "Citrix ICA Service" -ComputerName $computer | Restart-Service -Force
+Get-Service -Name "Citrix Desktop Service" -ComputerName $computer | Restart-Service -Force
+read-host "Success! Please wait 60 seconds, before attempting to connect. 
+Press enter key to exit script..."
